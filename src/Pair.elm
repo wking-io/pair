93ec5f59c2199fdc8fb3a02c6cdb48e1c7f5a7af
Will King <contact@wking.io>
2019-01-07 08:31:55 -0600

    Finalizing initial release of API and updating docs

--- src/Pair.elm
+++ src/Pair.elm
@@ -2,9 +2,9 @@ module Pair exposing
     ( Pair
     , from, branch, branchWith
     , left, right, merge
-    , map, map2, map3, map4, map5, map6, mapBoth, extend, swap
-    , andThen
-    , maybeTraverse, resultTraverse, taskTraverse
+    , map, andMap, map2, mapBoth, andMapBoth, mapBoth2, extend, modify, swap
+    , andThen, andThenBoth
+    , maybeMap, maybeUnwrap, resultMap, resultUnwrap, taskMap, taskUnwrap
     , equals
     )
 
@@ -28,20 +28,22 @@ module Pair exposing
 
 # Mapping Functions
 
-@docs map, map2, map3, map4, map5, map6, mapBoth, extend, swap
+@docs map, andMap, map2, mapBoth, andMapBoth, mapBoth2, extend, modify, swap
 
 
 # Chaining Functions
 
-@docs andThen
+@docs andThen, andThenBoth
 
 
 # Traversable Functions
 
-@docs maybeTraverse, resultTraverse, taskTraverse
+@docs maybeMap, maybeUnwrap, resultMap, resultUnwrap, taskMap, taskUnwrap
 
 
 # Boolean Functions
+
+@docs equals
 
 -}
 
@@ -51,15 +53,15 @@ import Task exposing (Task)
 {-| Represent two values that can computed in parallel. Similar to `Maybe` and `Result` it has a natural bias to the type on the right. However, it also includes functions like [`Pair.mapBoth`](Pair#mapBoth) to map functions to both types.
 
     -- Branching a number, running separate functions over each value, and merging them back into one value.
+
     computeThenCombine :: Int -> Int
     computeThenCombine int =
-      int
-        |> Pair.branchWith ((+) 2) ((+) 4)
-        |> Pair.mapBoth ((*) 2) ((*) 4)
-        |> Pair.merge (+)
+        int
+            |> Pair.branchWith ((+) 2) ((+) 4)
+            |> Pair.mapBoth ((_) 2) ((_) 4)
+            |> Pair.merge (+)
 
     computeThenCombine 2 -- > 32
-
     computeThenCombine 3 -- > 38
 
 -}
@@ -84,17 +86,17 @@ from =
 -}
 branch : a -> Pair a a
 branch a =
-    Pair a a
+    from a a
 
 
-{-| Construct a Pair by splitting one value of any type and mapping the passed in function over each side. Think of this as composing `branch` and `mapBoth`.
+{-| Construct a Pair by splitting one value of any type and mapping the passed in functions for each side. Think of this as composing `branch` and `mapBoth`.
 
     branchWith String.reverse String.length "Elm" -- > Pair "mlE" 3
 
 -}
 branchWith : (a -> b) -> (a -> c) -> a -> Pair b c
 branchWith lft rht a =
-    Pair (lft a) (rht a)
+    from (lft a) (rht a)
 
 
 {-| Get the value on the left side of the Pair.
@@ -143,75 +145,63 @@ map func (Pair a b) =
     Pair a (func b)
 
 
-{-| Apply a function that takes two values to the right side of two pairs.
+{-| Apply a functions that is on the right side of a Pair to the right value of another Pair. This is how to build `map2` and can be used to build `mapN` that can take as many Pairs as you want.
 
-    map2 max (from 2 4) (from 4 6) -- >  Pair 2 6
+    branch (+) (-) -- > Pair (number -> number -> number) (number -> number -> number)
+        |> andMapBoth (from 4 8) -- > Pair (number -> number) (number -> number)
+        |> andMapBoth (from 2 4) -- > Pair 6 4
 
 -}
-map2 : (b -> d -> e) -> Pair a b -> Pair c d -> Pair a e
-map2 func (Pair a b) (Pair c d) =
-    Pair a (func b d)
+andMap : Pair a b -> Pair c (b -> d) -> Pair a d
+andMap (Pair a b) (Pair c func) =
+    Pair a (func b)
 
 
-map3 : (b -> d -> f -> g) -> Pair a b -> Pair c d -> Pair e f -> Pair a g
-map3 func (Pair a b) (Pair c d) (Pair e f) =
-    Pair a (func b d f)
+{-| Apply a function that takes two values to the right side of two pairs.
+
+    map2 max (from 8 6) (from 2 4) -- > Pair 2 6
+
+-}
+map2 : (b -> d -> e) -> Pair a b -> Pair c d -> Pair c e
+map2 func pair1 pair2 =
+    from () func
+        |> andMap pair1
+        |> andMap pair2
 
 
-map4 : (b -> d -> f -> h -> i) -> Pair a b -> Pair c d -> Pair e f -> Pair g h -> Pair a i
-map4 func (Pair a b) (Pair c d) (Pair e f) (Pair g h) =
-    Pair a (func b d f h)
-
-
-map5 : (b -> d -> f -> h -> j -> k) -> Pair a b -> Pair c d -> Pair e f -> Pair g h -> Pair i j -> Pair a k
-map5 func (Pair a b) (Pair c d) (Pair e f) (Pair g h) (Pair i j) =
-    Pair a (func b d f h j)
-
-
-map6 : (b -> d -> f -> h -> j -> l -> m) -> Pair a b -> Pair c d -> Pair e f -> Pair g h -> Pair i j -> Pair k l -> Pair a m
-map6 func (Pair a b) (Pair c d) (Pair e f) (Pair g h) (Pair i j) (Pair k l) =
-    Pair a (func b d f h j l)
-
-
-{-| Apply a function to both the leeft and the right side of the pair.
+{-| Apply a function to both the left and the right side of a pair.
 
     branch 4
-        |> mapBoth ((*) 2) sqrt -- > Pair 8 2
+        |> mapBoth ((\*) 2) sqrt -- > Pair 8 2
 
 -}
 mapBoth : (a -> c) -> (b -> d) -> Pair a b -> Pair c d
 mapBoth lft rht (Pair a b) =
-    Pair (lft a) (rht b)
+    from (lft a) (rht b)
+
+
+{-| Apply functions that are on both sides of a Pair to the values of another Pair. This is how to build `mapBoth2` and can be used to build `mapBothN` that can take as many Pairs as you want.
+
+    branch (+) (-) -- > Pair (number -> number -> number) (number -> number -> number)
+        |> andMapBoth (from 4 8) -- > Pair (number -> number) (number -> number)
+        |> andMapBoth (from 2 4) -- > Pair 6 4
+
+-}
+andMapBoth : Pair a b -> Pair (a -> c) (b -> d) -> Pair c d
+andMapBoth (Pair a b) (Pair lft rgt) =
+    from (lft a) (rgt b)
 
 
 {-| Apply a function that takes two values to both the left and the right side of two pairs.
 
-    mapBoth2 max (from 2 4) (from 4 6) -- >  Pair 2 6
+    mapBoth2 min max (from 2 4) (from 4 6) -- > Pair 2 6
 
 -}
 mapBoth2 : (a -> c -> e) -> (b -> d -> f) -> Pair a b -> Pair c d -> Pair e f
-mapBoth2 lft rht (Pair a b) (Pair c d) =
-    Pair (lft a c) (rht b d)
-
-
-mapBoth3 : (a -> c -> e -> g) -> (b -> d -> f -> h) -> Pair a b -> Pair c d -> Pair e f -> Pair g h
-mapBoth3 lft rht (Pair a b) (Pair c d) (Pair e f) =
-    Pair (lft a c e) (rht b d f)
-
-
-mapBoth4 : (a -> c -> e -> g -> i) -> (b -> d -> f -> h -> j) -> Pair a b -> Pair c d -> Pair e f -> Pair g h -> Pair i j
-mapBoth4 lft rht (Pair a b) (Pair c d) (Pair e f) (Pair g h) =
-    Pair (lft a c e g) (rht b d f h)
-
-
-mapBoth5 : (a -> c -> e -> g -> i -> k) -> (b -> d -> f -> h -> j -> l) -> Pair a b -> Pair c d -> Pair e f -> Pair g h -> Pair i j -> Pair k l
-mapBoth5 lft rht (Pair a b) (Pair c d) (Pair e f) (Pair g h) (Pair i j) =
-    Pair (lft a c e g i) (rht b d f h j)
-
-
-mapBoth6 : (a -> c -> e -> g -> i -> k -> m) -> (b -> d -> f -> h -> j -> l -> n) -> Pair a b -> Pair c d -> Pair e f -> Pair g h -> Pair i j -> Pair k l -> Pair m n
-mapBoth6 lft rht (Pair a b) (Pair c d) (Pair e f) (Pair g h) (Pair i j) (Pair k l) =
-    Pair (lft a c e g i k) (rht b d f h j l)
+mapBoth2 lft rht pair1 pair2 =
+    from lft rht
+        |> andMapBoth pair1
+        |> andMapBoth pair2
 
 
 {-| Apply a function that will be passed both the left and right values of the pair to transform the right side of the Pair.
@@ -220,42 +210,110 @@ mapBoth6 lft rht (Pair a b) (Pair c d) (Pair e f) (Pair g h) (Pair i j) (Pair k 
         |> extend (+) -- > Pair 4 8
 
 -}
-extend : (Pair a b -> c) -> Pair a b -> Pair a c
-extend f ((Pair a b) as pair) =
-    Pair a (f pair)
+extend : (a -> b -> c) -> Pair a b -> Pair a c
+extend f (Pair a b) =
+    from a (f a b)
 
 
-swap : (a -> c) -> (b -> d) -> Pair a b -> Pair d c
-swap lft rht (Pair a b) =
-    Pair (rht b) (lft a)
+{-| Apply a function that will be passed both the left and right values of the pair to transform the left side of the Pair.
+
+    branch 4
+        |> extend (+) -- > Pair 8 4
+
+-}
+modify : (a -> b -> c) -> Pair a b -> Pair c b
+modify f (Pair a b) =
+    from (f a b) b
 
 
-andThen : (a -> Pair a c) -> Pair a b -> Pair a c
+{-| Swap both sides of a Pair.
+
+    from "Left" "Right"
+        |> swap -- > Pair "Right" "Left"
+
+-}
+swap : Pair a b -> Pair b a
+swap (Pair a b) =
+    from b a
+
+
+{-| Swap both sides of a Pair mapping each side with the passed in function first. Think of this as `mapBoth` then `swap`.
+
+    from "Left" "Right"
+        |> swapWith String.length String.toUpper -- > Pair "RIGHT" 4
+
+-}
+swapWith : (a -> c) -> (b -> d) -> Pair a b -> Pair d c
+swapWith lft rht (Pair a b) =
+    from (rht b) (lft a)
+
+
+{-| Map over a Pair with a function that takes in the right value and returns a new Pair. The left value returned from the passed in function will replace the previous left value. If you want to have control over how the left value changes check out `andThenBoth`.
+
+    from "Left" "Right"
+        |> chain (\x -> from "newLeft" (String.reverse x)) -- > Pair "newLeft" "thgiR"
+
+-}
+andThen : (b -> Pair c d) -> Pair a b -> Pair c d
 andThen func (Pair a b) =
     func b
-        |> mapBoth (always a) identity
 
 
-andThenWith : (a -> b -> Pair c d) -> Pair a b -> Pair c d
-andThenWith func (Pair a b) =
+{-| Map over a Pair with a function that takes in the left and right values and returns a new Pair.
+
+    from "Left" "Right"
+        |> chain (\x y -> from (String.length) (String.reverse x)) -- > Pair 4 "thgiR"
+
+-}
+andThenBoth : (a -> b -> Pair c d) -> Pair a b -> Pair c d
+andThenBoth func (Pair a b) =
     func a b
 
 
-maybeTraverse : Pair a (Maybe b) -> Maybe (Pair a b)
-maybeTraverse (Pair a maybe) =
-    Maybe.map (\b -> Pair a b) maybe
+{-| Map over the right value in a Pair with a function that returns a `Maybe` and return a `Maybe` with the mapped `Pair` inside instead a `Pair` with a `Maybe` inside.
+
+    -- With regular map.
+    branch [ 1, 2, 3 ]
+        |> map List.head -- > Pair [ 1, 2, 3 ] (Just 1)
+
+    -- With maybeMap.
+    branch [ 1, 2, 3 ]
+        |> maybeMap List.head -- > Just (Pair [ 1, 2, 3 ] 1)
+
+-}
+maybeMap : (b -> Maybe c) -> Pair a b -> Maybe (Pair a c)
+maybeMap func ((Pair a b) as pair) =
+    func b
+        |> Maybe.map (\c -> Pair a c)
 
 
-resultTraverse : Pair a (Result err b) -> Result err (Pair a b)
-resultTraverse (Pair a result) =
-    Result.map (\b -> Pair a b) result
+resultMap : (b -> Result err c) -> Pair a b -> Result err (Pair a c)
+resultMap func ((Pair a b) as pair) =
+    func b
+        |> Result.map (\c -> Pair a c)
 
 
-taskTraverse : Pair a (Task err b) -> Task err (Pair a b)
-taskTraverse (Pair a task) =
-    Task.map (\b -> Pair a b) task
+taskMap : (b -> Task err c) -> Pair a b -> Task err (Pair a c)
+taskMap func ((Pair a b) as pair) =
+    func b
+        |> Task.map (\c -> Pair a c)
 
 
-equals : (( a, b ) -> ( c, d ) -> Bool) -> Pair a b -> Pair c d -> Bool
+maybeUnwrap : Pair a (Maybe b) -> Maybe (Pair a b)
+maybeUnwrap =
+    maybeMap identity
+
+
+resultUnwrap : Pair a (Result err b) -> Result err (Pair a b)
+resultUnwrap =
+    resultMap identity
+
+
+taskUnwrap : Pair a (Task err b) -> Task err (Pair a b)
+taskUnwrap =
+    taskMap identity
+
+
+equals : (a -> b -> c -> d -> Bool) -> Pair a b -> Pair c d -> Bool
 equals pred (Pair a b) (Pair c d) =
-    pred ( a, b ) ( c, d )
+    pred a b c d
